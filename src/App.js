33de8872eb0608926d98fe9d46a707ba3cf3177e
andylukak <alexandru.tirim@yahoo.com>
2022-02-25 18:57:50 +0200

    Lazy load components

--- src/App.js
+++ src/App.js
@@ -2,51 +2,51 @@ import './App.scss';
 
 import { Routes, Route } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
+import React, { lazy } from 'react';
 
+import Home from './pages/Home/Home';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Home from './pages/Home/Home';
-import Blog from './pages/Blog/Blog';
-import Login from './pages/Login/Login';
-import Admin from './pages/Admin/Admin';
-import PostManager from './pages/PostManager/PostManager';
-import PostWriter from './pages/PostWriter/PostWriter';
-import EditPost from './components/EditPost/EditPost';
 
-import SinglePost from './pages/SinglePost/SinglePost';
-import TagOverview from './pages/TagOverview/TagOverview';
+const Blog = lazy(() => import('./pages/Blog/Blog'));
+const SinglePost = lazy(() => import('./pages/SinglePost/SinglePost'));
+const TagOverview = lazy(() => import('./pages/TagOverview/TagOverview'));
+const Login = lazy(() => import('./pages/Login/Login'));
+const Admin = lazy(() => import('./pages/Admin/Admin'));
+const PostManager = lazy(() => import('./pages/PostManager/PostManager'));
+const PostWriter = lazy(() => import('./pages/PostWriter/PostWriter'));
+const EditPost = lazy(() => import('./components/EditPost/EditPost'));
 
 function App() {
 	return (
 		<div className="App">
 			<Helmet>
 				<title>Alex Tirim </title>
-				<meta name="description" content="Alex Tirim - free content to learn about programming and everything related to the web" />
+				<meta
+					name="description"
+					content="Alex Tirim - free content to learn about programming and everything related to the web"
+				/>
 			</Helmet>
 			<Header />
-			<Routes>
-				<Route exact path="/" element={<Home />} />
-				<Route path="/blog" element={<Blog />} />
-				<Route path="/:slug" element={<SinglePost />} />
-				<Route path="/tags/:tag" element={<TagOverview />} />
-				<Route
-					exact
-					path="/admin-login"
-					element={
-						<Login />
-					}
-				/>
-				<Route exact path="/admin" element={<Admin />}>
-					<Route path="manager" element={<PostManager />}>
-						<Route path=":postID" element={<EditPost />} />
+			<React.Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route path="/blog" element={<Blog />} />
+					<Route path="/:slug" element={<SinglePost />} />
+					<Route path="/tags/:tag" element={<TagOverview />} />
+					<Route exact path="/admin-login" element={<Login />} />
+					<Route exact path="/admin" element={<Admin />}>
+						<Route path="manager" element={<PostManager />}>
+							<Route path=":postID" element={<EditPost />} />
+						</Route>
+						<Route path="new" element={<PostWriter />} />
 					</Route>
-					<Route path="new" element={<PostWriter />} />
-				</Route>
-				<Route path="*" element={<Home />} />
-			</Routes>
+					<Route path="*" element={<Home />} />
+				</Routes>
+			</React.Suspense>
 			<Footer />
 		</div>
 	);
 }
 
-export default (App);
+export default App;
